d5d2ac1098eb187201009ae422d2fc6a9414672b
xmflswood <774898896@qq.com>
2019-01-31 15:29:19 +0800

    update

--- check.js
+++ check.js
@@ -1,35 +1,57 @@
 const fs = require('fs')
 const request = require('request-promise')
-let url = 'https://www.baidu.com'
-const ls = JSON.parse(fs.readFileSync('./result.json'))
+let url = 'http://m.yuetengzs.com/Votephoto/addVote.html'
+const ls = JSON.parse(fs.readFileSync('./list.json'))
+let temp = JSON.parse(fs.readFileSync('./temp.json'))
+if (temp.length === 0) {
+  temp = ls;
+  fs.writeFileSync('./temp.json', JSON.stringify(temp))
+}
+success = 0
 let r = []
 async function test() {
-  for (let i = 0; i < ls.length; i ++) {
-    console.log((i + 1) + '/' + ls.length)
-    let t = await check(ls[i])
+  for (let i = temp.length; i > 0; i--) {
+    console.log('剩余' + temp.length)
+    let t = false
+    try {
+      t = await check(temp[i])
+    } catch(e) {
+     console.log(e)
+    }
+    
     console.log(t)
     if (t) {
-       r.push(ls[i])
+       r.push(temp[i])
     }
+    temp.splice(0, 1)
+    i--;
+    fs.writeFileSync('./temp.json', JSON.stringify(temp))
   }
   console.log(r)
   fs.writeFileSync('./result.json', JSON.stringify(r))
 }
-test()
+// test()
 function check(s) {
+  console.log(s)
   s = 'http://' + s
   let timeout = 5000
-  console.log(s)
   let opt = {
     proxy: s,
-    method: 'GET',
+    method: 'POST',
     url: url,
-    timeout: timeout
+    timeout: timeout,
+    formData: {
+      id: 76
+    }
   }
   return new Promise((resolve, reject) => {
    let flag = false
    request(opt).then(
-    (res) => { 
+    (res) => {
+     if (JSON.parse(res).status === 1) {
+       success++
+       console.log(`成功刷票${success}次`)
+     } 
      flag = true
      resolve(true)
     }
@@ -40,10 +62,11 @@ function check(s) {
     }
    )
    setTimeout(() => {
-     if (!flag) resolve(false)
+     if (!flag) {
+       resolve(false)
+     }
    }, timeout)
   })
-  
 }
 function freezeTime(t) {
   return new Promise((resolve, reject) => {
@@ -60,3 +83,8 @@ function getProxy(s) {
     port: +s.substring(i+1)
   }
 }
+async function testt () {
+  let a = await check('139.199.181.200:8080')
+  console.log(a)
+}
+test()
